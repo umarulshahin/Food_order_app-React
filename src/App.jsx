09a819f81b7c164e73ac6c733274cc991965fcf5
umarulshahin <akkushahin666@gmail.com>
2024-05-04 23:31:25 +0530

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -32,13 +32,14 @@ const RestaurantCard=(props)=>{
     cuisines,
     avgRating,
     costForTwo,
+    cloudinaryImageId,
     sla
 
   }=resdata?.info
   return (
 
     <div className="res_card" style={{backgroundColor:"#f0f0f0"}}>
-      <img className="res_logo" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+resdata.info.cloudinaryImageId} alt="" />
+      <img className="res_logo" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId} alt="" />
       <h3>{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
       <h4>{avgRating}stars</h4>
